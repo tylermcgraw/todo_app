b96fd3b16a339e321963b83de689cab44c3a88ca
tylermcgraw <=>
2025-01-23 10:13:27 -0500

    Deployment

--- schema.sql
+++ schema.sql
@@ -1,18 +1,21 @@
-CREATE TABLE todolists(
+CREATE TABLE todolists (
   id serial PRIMARY KEY,
-  title varchar(100) UNIQUE NOT NULL,
+  title text NOT NULL UNIQUE,
   username text NOT NULL
 );
 
-CREATE TABLE todos(
+CREATE TABLE todos (
   id serial PRIMARY KEY,
-  title varchar(100) NOT NULL,
+  title text NOT NULL,
   done boolean NOT NULL DEFAULT false,
   username text NOT NULL,
-  todolist_id integer NOT NULL REFERENCES todolists(id) ON DELETE CASCADE
-)
+  todolist_id integer
+    NOT NULL
+    REFERENCES todolists (id)
+    ON DELETE CASCADE
+);
 
-CREATE TABLE users(
+CREATE TABLE users (
   username text PRIMARY KEY,
   password text NOT NULL
 );
